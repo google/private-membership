6647e2fe2b6e6628f5ecad1b98ef40cea416a54d
Joon Young Seo <jyseo@google.com>
2024-05-06 23:01:53 +0000

    Update code

--- research/okvs/bandokvs/band_okvs.h
+++ research/okvs/bandokvs/band_okvs.h
@@ -26,52 +26,52 @@ inline T CreateBand(oc::span<oc::block> blocks,
 template<>
 inline uint<1> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<1>(((blocks[0]).get<__uint128_t>(0) & high_mask) | 1);
+  return uint < 1 > (((blocks[0]).get<__uint128_t>(0) & high_mask) | 1);
 }
 
 template<>
 inline uint<2> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<2>((blocks[0]).get<__uint128_t>(0) | 1,
-                 (blocks[1]).get<__uint128_t>(0) & high_mask);
+  return uint < 2 > ((blocks[0]).get<__uint128_t>(0) | 1,
+      (blocks[1]).get<__uint128_t>(0) & high_mask);
 }
 
 template<>
 inline uint<3> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<3>((blocks[0]).get<__uint128_t>(0) | 1,
-                 (blocks[1]).get<__uint128_t>(0),
-                 (blocks[2]).get<__uint128_t>(0) & high_mask);
+  return uint < 3 > ((blocks[0]).get<__uint128_t>(0) | 1,
+      (blocks[1]).get<__uint128_t>(0),
+      (blocks[2]).get<__uint128_t>(0) & high_mask);
 }
 
 template<>
 inline uint<4> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<4>((blocks[0]).get<__uint128_t>(0) | 1,
-                 (blocks[1]).get<__uint128_t>(0),
-                 (blocks[2]).get<__uint128_t>(0),
-                 (blocks[3]).get<__uint128_t>(0) & high_mask);
+  return uint < 4 > ((blocks[0]).get<__uint128_t>(0) | 1,
+      (blocks[1]).get<__uint128_t>(0),
+      (blocks[2]).get<__uint128_t>(0),
+      (blocks[3]).get<__uint128_t>(0) & high_mask);
 }
 
 template<>
 inline uint<5> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<5>((blocks[0]).get<__uint128_t>(0) | 1,
-                 (blocks[1]).get<__uint128_t>(0),
-                 (blocks[2]).get<__uint128_t>(0),
-                 (blocks[3]).get<__uint128_t>(0),
-                 (blocks[4]).get<__uint128_t>(0) & high_mask);
+  return uint < 5 > ((blocks[0]).get<__uint128_t>(0) | 1,
+      (blocks[1]).get<__uint128_t>(0),
+      (blocks[2]).get<__uint128_t>(0),
+      (blocks[3]).get<__uint128_t>(0),
+      (blocks[4]).get<__uint128_t>(0) & high_mask);
 }
 
 template<>
 inline uint<6> CreateBand(oc::span<oc::block> blocks,
                           __uint128_t high_mask) {
-  return uint<6>((blocks[0]).get<__uint128_t>(0) | 1,
-                 (blocks[1]).get<__uint128_t>(0),
-                 (blocks[2]).get<__uint128_t>(0),
-                 (blocks[3]).get<__uint128_t>(0),
-                 (blocks[4]).get<__uint128_t>(0),
-                 (blocks[5]).get<__uint128_t>(0) & high_mask);
+  return uint < 6 > ((blocks[0]).get<__uint128_t>(0) | 1,
+      (blocks[1]).get<__uint128_t>(0),
+      (blocks[2]).get<__uint128_t>(0),
+      (blocks[3]).get<__uint128_t>(0),
+      (blocks[4]).get<__uint128_t>(0),
+      (blocks[5]).get<__uint128_t>(0) & high_mask);
 }
 
 template<typename T>
@@ -126,7 +126,7 @@ inline void GenBandsAndValues(int n, const oc::block* keys,
                   (static_cast<__uint128_t>(1) << (band_length % 128)) - 1;
   uint32_t divisor = okvs_length - band_length + 1;
   oc::AES aes(oc::ZeroBlock);
-  #pragma GCC unroll 16
+#pragma GCC unroll 16
   for (int i = 0; i < n; ++i) {
 
     oc::block block = aes.hashBlock(keys[i]);
@@ -239,9 +239,7 @@ class BandOkvs {
   }
 
   template<typename T, typename V>
-  bool Encode(const oc::block* keys,
-              const V* values,
-              V* out) {
+  bool Encode(const oc::block* keys, const V* values, V* out) {
     int n = num_eqns_;
     auto time_start = std::chrono::steady_clock::now();
     oc::PRNG prng(prng_seed_);
@@ -249,7 +247,7 @@ class BandOkvs {
     BandAndValue<T, V>* bands =
         (BandAndValue<T, V>*) malloc(n * sizeof(BandAndValue<T, V>));
     GenBandsAndValues<T, V>(n, keys, values, num_vars_,
-                               band_length_, bands);
+                            band_length_, bands);
     //std::sort(bands, bands + n, lessthan());
     boost::sort::spreadsort::integer_sort(bands, bands + n,
                                           rightshift(), lessthan());
@@ -257,35 +255,30 @@ class BandOkvs {
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>((time_end
         - time_start)).count() << std::endl;
 
-    
     bool encoded = Encode<T, V>(bands, out);
     //free(bands);
     return encoded;
   }
 
   template<typename T, typename V>
-  inline void Decode(const Band<T>* bands,
-                     const V* okvs,
-                     V* out) {
+  inline void Decode(const Band<T>* bands, const V* okvs, V* out) {
     int n = num_eqns_;
     for (int i = 0; i < n; ++i) {
       int band_start = bands[i].BandStart();
       __m512i res = DoXor(band_start, bands[i].RawBand(), okvs, band_length_);
       __m128i res2 = DoXor512(res);
 
-      _mm_storeu_si128((__m128i*) &out[bands[i].Index()], res2);
+      _mm_storeu_si128((__m128i * ) & out[bands[i].Index()], res2);
     }
   }
 
   template<typename T, typename V>
-  void Decode(const oc::block* keys,
-              const V* okvs,
-              V* out) {
+  void Decode(const oc::block* keys, const V* okvs, V* out) {
     int n = num_eqns_;
     Band<T>* bands = (Band<T>*) malloc(n * sizeof(Band<T>));
 
     GenBands<T>(n, keys, num_vars_, band_length_,
-                   bands);
+                bands);
 
     boost::sort::spreadsort::integer_sort(bands, bands + n,
                                           rightshift(), lessthan());
@@ -295,23 +288,24 @@ class BandOkvs {
   }
 
  private:
-  inline static constexpr __mmask8 table_[16] = {
-      0,
-      3,
-      12,
-      15,
-      48,
-      51,
-      60,
-      63,
-      192,
-      195,
-      204,
-      207,
-      240,
-      243,
-      252,
-      255
+  inline static constexpr __mmask8
+  table_[16] = {
+    0,
+        3,
+        12,
+        15,
+        48,
+        51,
+        60,
+        63,
+        192,
+        195,
+        204,
+        207,
+        240,
+        243,
+        252,
+        255
   };
 
   // Xors values in range [i, i + length) according to the mask defined by
@@ -324,9 +318,9 @@ class BandOkvs {
       return res;
     }
 
-    #pragma GCC unroll 8
+#pragma GCC unroll 8
     for (int j = 0, k = 0; j < length; j += 8, k++) {
-      uint8_t raw_band_mask = *((uint8_t*) (&raw_band) + k);
+      uint8_t raw_band_mask = *((uint8_t * )(&raw_band) + k);
 
       __mmask8 mask = table_[raw_band_mask & 0b1111];
       res = _mm512_xor_epi64(
@@ -360,7 +354,7 @@ class BandOkvs {
       __m512i res = DoXor(i, reduced_matrix[i], reduced_values, band_length_);
       __m128i res2 = DoXor512(res);
 
-      _mm_storeu_si128((__m128i*) &reduced_values[i], res2);
+      _mm_storeu_si128((__m128i * ) & reduced_values[i], res2);
     }
   }
 
